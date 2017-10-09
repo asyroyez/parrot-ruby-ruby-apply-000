a9168f3b1bf2f39f439c19376ca26909ff19aa75
Andrey Syroyezhkin <asyroyez@gmail.com>
2017-10-09 15:58:26 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(*phrase)
-  @phrase = phrase.to_s
+  @phrase = phrase
   if phrase.length == 0
     puts "Squawk!"
     return "Squawk!"
